07bdc75ea0cf29ee57644984313c0c57f4b903a1
EmChes <emches1976@gmail.com>
2023-02-01 13:55:27 +0700

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -1,6 +1,6 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://localhost/bkfresh/');
+if(!defined('base_url')) define('base_url','http://ec2-52-221-206-198.ap-southeast-1.compute.amazonaws.com/CO3105_BKFRESH_HK221/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
